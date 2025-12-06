c40615bd5c6adffc0da21154a54ccf6f45f6197d
Андрей Шитиков <fox@MacBook-Pro-Andrej.local>
2025-12-06 14:58:00 +0300

    Build mini app for GitHub Pages

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/https://github.com/andriffter-lang/thule_hub_bot.git/",
+  base: "/thule_hub_bot/",
   build: {
     outDir: "../docs",     // собирать сразу в /docs в корне репо
     emptyOutDir: true,     // очищать /docs перед сборкой
